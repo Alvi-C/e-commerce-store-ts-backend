cbc75b33a09f06ef8ecc3043d3e585ebf355e15d
Alvi Chowdhury <alvic5050@gmail.com>
2024-05-21 00:00:22 +0600

    [Done] Create product schema and model

--- src/app/modules/product/product.types.ts
+++ src/app/modules/product/product.types.ts
@@ -1,14 +1,14 @@
-type Variant = {
+export type Variant = {
   type: string;
   value: string;
 };
 
-type Inventory = {
+export type Inventory = {
   quantity: number;
   inStock: boolean;
 };
 
-type Product = {
+export type Product = {
   name: string;
   description: string;
   price: number;
