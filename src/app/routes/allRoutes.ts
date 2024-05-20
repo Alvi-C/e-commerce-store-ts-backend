36f4871b3ca03852d13f85b60cbe6c6bf75f9ec1
Alvi Chowdhury <alvic5050@gmail.com>
2024-05-20 22:56:06 +0600

    [Done] project setup & database connectivity

---
+++ src/app/routes/allRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+// test route
+router.get('/test', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: '🎉🎉 Test route is working! 🎉🎉',
+  });
+});
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+import config from './app/config'
+import app from './app'
+
+const port = config.port
+const databaseUrl = config.database_url
+
+async function server(): Promise<void> {
+	try {
+		await mongoose.connect(databaseUrl as string)
+		console.log('🌼 Database connected 🌼')
+
+		app.listen(port, () => {
+			console.log(`🚀🚀 Server is running on port ${port} 🚀🚀`)
+		})
+	} catch (error) {
+		console.log('🔥 Faild to connect database 🔥')
+		console.error('Server failed to start:', error)
+		process.exit(1)
+	}
+}
+
+// call the server function
+server().catch(error => {
+	console.error('Server failed to start:', error)
+	process.exit(1)
+})
