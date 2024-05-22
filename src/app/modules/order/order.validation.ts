32be0e526161841f8fd0b3208ccfdc267b0ff40c
Alvi Chowdhury <alvic5050@gmail.com>
2024-05-22 14:21:35 +0600

    [Update] order validation

--- src/app/modules/order/order.validation.ts
+++ src/app/modules/order/order.validation.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const orderValidationSchema = z.object({
-  email: z.string().trim().min(2, 'Email is required!'),
+  email: z.string().trim().min(2, 'Email is required!').email(),
   productId: z.string().trim().min(5, 'Product ID is required!'),
   price: z.number().positive('Price must be positive number!'),
   quantity: z.number().positive('Quantity must be positive number!'),
