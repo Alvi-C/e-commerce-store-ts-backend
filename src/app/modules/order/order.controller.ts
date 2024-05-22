87908059b28c7e233493c01d0113b93f8bc4d1e7
Alvi Chowdhury <alvic5050@gmail.com>
2024-05-22 14:41:02 +0600

    [Done & tested] get-all-orders route, service & controller

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -4,6 +4,7 @@ import orderValidationSchema from './order.validation';
 import { z } from 'zod';
 import { ProductServices } from '../product/product.services';
 
+//--------> Create a new order <--------
 const createOrder = async (req: Request, res: Response) => {
   try {
     // get data from request body
@@ -16,6 +17,8 @@ const createOrder = async (req: Request, res: Response) => {
     const productAvailability = await ProductServices.getProductAvailability(
       zodParsedData.productId,
     );
+
+    // Check sufficient product quantity before creating an order
     if (zodParsedData.quantity > productAvailability.quantity) {
       return res.status(400).json({
         success: false,
@@ -64,6 +67,37 @@ const createOrder = async (req: Request, res: Response) => {
   }
 };
 
+//--------> Get all orders <--------
+const getAllOrders = async (req: Request, res: Response) => {
+  try {
+    // call service function to get all orders
+    const result = await OrderServices.getAllOrdersFromDB();
+
+    // Check if orders were found
+    if (!result || result.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: 'Order not found!',
+      });
+    }
+
+    // Respond with success if orders were found
+    res.status(200).json({
+      success: true,
+      message: 'Orders fetched successfully!',
+      data: result,
+    });
+  } catch (error) {
+    const typedError = error as Error;
+    res.status(500).json({
+      success: false,
+      message: 'Product fetch failed',
+      error: typedError,
+    });
+  }
+};
+
 export const OrderControllers = {
   createOrder,
+  getAllOrders,
 };
--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -3,6 +3,9 @@ import { OrderControllers } from './order.controller';
 
 const orderRouter = express.Router();
 
-orderRouter.route('/api/orders').post(OrderControllers.createOrder);
+orderRouter
+  .route('/api/orders')
+  .post(OrderControllers.createOrder)
+  .get(OrderControllers.getAllOrders);
 
 export default orderRouter;
